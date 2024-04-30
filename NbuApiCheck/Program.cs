e5849693be7c174486f6ef688d322eaf2bc8b182
Ansmv <63971889+Ansmv@users.noreply.github.com>
2024-04-30 14:15:30 +0300

    feat: fetching main params from appsettings.json

---
+++ NbuApiCheck/Program.cs
@@ -0,0 +1,36 @@
+﻿using CurrencyDataLibrary;
+using CurrencyDataLibrary.ApiCommunication;
+using CurrencyDataLibrary.Models;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration config = builder.Build();
+int delayInSeconds = int.Parse(config["DelayInSeconds"]);
+string dataFormat = config["DataFormat"];
+string dataStoragePath = config["DataStoragePath"];
+
+JsonFetcher jsonFetcher = new(
+           new HttpClientWrapper(),
+           new CacheManager<string>(
+               new TimeSpan(0, 10, 0)
+           ),
+           "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+       );
+JsonToCurrencyDataProcessor processor = new();
+CurrencyAPIClient currencyAPIClient = new(jsonFetcher, processor);
+
+for (int i = 0; i < 60; i++) // 60 iterations for 5 minutes
+{
+    List<CurrencyData> currencyData = await currencyAPIClient.FetchCurrencyData();
+    Console.WriteLine($"Successfully fetched: {currencyData.Count} currencies");
+
+    DataSaver.SaveToFile(currencyData, dataFormat, dataStoragePath);
+    Console.WriteLine("Saved to a file");
+
+    await Task.Delay(delayInSeconds * 1000); // Delay for 5 seconds before next iteration
+}
+
+Console.WriteLine("Testing complete.");
