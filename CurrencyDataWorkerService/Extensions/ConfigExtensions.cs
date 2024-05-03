09f68a8f7d2c866a6cace3e78e1bb025e4bd0700
Ansmv <63971889+Ansmv@users.noreply.github.com>
2024-05-03 13:13:24 +0300

    refactor: extention methods made more explicit

---
+++ CurrencyDataWorkerService/Extensions/ConfigExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyDataWorkerService.Extensions
+{
+    public static class ConfigExtensions
+    {
+        public static IConfiguration LoadConfiguration(string basePath, string fileName) =>
+            new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile(fileName, optional: false, reloadOnChange: true)
+                .Build();
+    }
+}
